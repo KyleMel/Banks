c17c6b2f01d95776abaa3a73e25633931cdd0eec
Kyle Melton <KyleMeltonCodes@gmail.com>
2021-09-01 18:41:17 -0500

    start of project

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banks
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("kyle", 1000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial Balance.");
+            account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+            Console.WriteLine(account.Balance);
+            account.MakeDeposit(100, DateTime.Now, "Friend paid me back");
+            Console.WriteLine(account.Balance);
+            Console.WriteLine(account.GetAccountHistory());
+
+            var giftCard = new GiftCardAccount("gift card", 100, 50);
+            giftCard.MakeWithdrawal(20, DateTime.Now, "get expensive coffee");
+            giftCard.MakeWithdrawal(50, DateTime.Now, " buy groceries");
+            giftCard.PerformMonthEndTransactions();
+
+            giftCard.MakeDeposit(27.50m, DateTime.Now, "add some additional spedning money");
+            Console.WriteLine(giftCard.GetAccountHistory());
+
+            var savings = new InterestEarningAccount("saving account", 10000);
+            savings.MakeDeposit(750, DateTime.Now, "Save some money");
+            savings.MakeDeposit(1250, DateTime.Now, "add more savings");
+            savings.MakeWithdrawal(250, DateTime.Now, "Needed to pay monthly bills");
+            savings.PerformMonthEndTransactions();
+            Console.WriteLine(savings.GetAccountHistory());
+
+            var lineOfCredit = new LineOfCreditAccount("line of credit", 0);
+
+            lineOfCredit.MakeWithdrawal(1000m, DateTime.Now, "Take out monthly advance");
+            lineOfCredit.MakeDeposit(50m, DateTime.Now, "Pay back small amount");
+            lineOfCredit.MakeWithdrawal(5000m, DateTime.Now, "Emergency funds for repairs");
+            lineOfCredit.MakeDeposit(150m, DateTime.Now, "Partial restoration of repairs");
+            lineOfCredit.PerformMonthEndTransactions();
+            Console.WriteLine(lineOfCredit.GetAccountHistory());
+        }
+    }
+}
